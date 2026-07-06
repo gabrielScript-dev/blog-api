40d0050df2cd3ba6cd392ebbce48475413a73ea6
Gabriel Pereira <gabriel.script@gmail.com>
2026-07-06 23:23:19 +0000

    refactor(users): convert UserRetrieveAPIView to use generics.RetrieveUpdateDestroyAPIView

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from accounts.views import UserCreateAPIView, UserRetrieveAPIView
+from accounts.views import UserCreateAPIView, UserRetrieveUpdateDestroyAPIView
 
 
 urlpatterns = [
     path(route="accounts/", view=UserCreateAPIView.as_view(), name="account-create-view"),
-    path(route="accounts/<int:pk>", view=UserRetrieveAPIView.as_view(), name="account-detail-view")
+    path(route="accounts/<int:pk>", view=UserRetrieveUpdateDestroyAPIView.as_view(), name="account-detail-view")
 ]
